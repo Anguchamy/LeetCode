5ebcc9a20b789fabb5f444000d82acb57a3c9278
Anguchamy <41102412+Anguchamy@users.noreply.github.com>
2024-01-14 00:15:16 +0530

    Time: 15 ms (24.76%) | Memory: 43.1 MB (33.35%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,31 +1,24 @@
 class Solution {
     public String countAndSay(int n) {
+        String s = "1";
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
         HashMap<Integer, String> map = new HashMap<>();
-		map.put(1,"1");
-		map.put(2,"11");
-        int arr[] = new int[10];
-		for(int i=3;i<=30;i++) {
-			String temp = map.get(i-1);
-			String tempres = "";
-			arr[temp.charAt(0)-'0']++;
-			for(int tempi=1;tempi<temp.length();tempi++) {
-				if(temp.charAt(tempi) == temp.charAt(tempi-1)) {
-					arr[temp.charAt(tempi)-'0']++;
-				} else {
-					tempres += (arr[temp.charAt(tempi-1) - '0'] + "" + temp.charAt(tempi-1));
-					arr[temp.charAt(tempi-1) - '0'] = 0;
-					arr[temp.charAt(tempi) - '0']++;
-				}
-
-                if(tempi == temp.length()-1) {
-                    tempres += arr[temp.charAt(tempi) - '0'] + "" + temp.charAt(tempi);
-                    arr[temp.charAt(tempi) - '0'] = 0;
+        map.put(1, "1");
+        for(int i=2;i<=30;i++) {
+            sb = new StringBuilder();
+            for(int j=0;j<s.length();j++) {
+                count = 1;
+                while(j+1 < s.length() && s.charAt(j) == s.charAt(j+1)) {
+                    j++;
+                    count++;
                 }
-			}
 
-			map.put(i, tempres);
-		}
-
-		return map.get(n);
+                sb.append(count).append(s.charAt(j));
+            }
+            s = sb.toString();
+            map.put(i, s);
+        }
+        return map.get(n);
     }
 }
