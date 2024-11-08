853c5b3133f48b8e1d420f5f2d43fffcf21f0730
Anguchamy <41102412+Anguchamy@users.noreply.github.com>
2024-11-08 21:58:19 +0530

    Time: 6 ms (57.84%) | Memory: 41.7 MB (94.05%) - LeetSync

---
+++ 768-partition-labels/partition-labels.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        int[] map = new int[123]; // Frequency map
+        for(char c: s.toCharArray()){
+            map[c]++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        Set<Character> set = new HashSet<>(); 
+        int size = 0;
+
+        for(char c: s.toCharArray()){
+            size++;
+            set.add(c); 
+            if(--map[c] == 0){
+                set.remove(c); 
+            }
+            if(set.isEmpty()){ 
+                result.add(size);
+                size = 0; // Reset the size
+            }
+        }
+
+        return result;
+    }
+}
